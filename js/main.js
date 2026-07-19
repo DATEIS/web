1c36984a8d56dd45e3bf4b9ed393cd4b398b09e2
LeeFoS6092 <LeeFoS6092>
2026-07-19 18:03:48 +0300

    Add base page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const disappearingText = document.getElementById('disapText');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 50) {
+        disappearingText.style.opacity = 0;
+    } else {
+        disappearingText.style.opacity = 1;
+    }
+})
